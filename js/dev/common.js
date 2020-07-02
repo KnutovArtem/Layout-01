44db2eb8f16752c413c80d5578a5b6d885dcb4cf
ArtemKnutov <arv.knutov@gmail.com>
2020-07-02 19:04:50 +0300

    Update

--- js/dev/common.js
+++ js/dev/common.js
@@ -1,13 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
-    const header       = document.querySelector('.header');
-    const wrapper      = document.querySelector('.main-wrapper');
-    const dropdownMenu = document.querySelector('.dropdown-menu');
+    const header         = document.querySelector('.wrapper-header');
+    const headerMenuItem = document.querySelectorAll('.header-menu__item');
+    const wrapper        = document.querySelector('.main-wrapper');
+    const dropdownMenu   = document.querySelector('.dropdown-menu');
 
     /* ---------------------------------------------------------------*/
     const fixedMenu = () => {
-
         window.addEventListener('scroll', () => {
 
             let scrollUp = document.documentElement.scrollTop;
@@ -33,31 +33,59 @@ window.addEventListener('DOMContentLoaded', () => {
         request.addEventListener("readystatechange", () => {
 
             if (request.readyState === 4 && request.status === 200) {
-
                 const data = JSON.parse(request.responseText);
 
-                data.forEach((elem) => {
+                /*Functions */
+                const showSubmenuItems = (elemData) => {
+                    dropdownMenu.innerHTML = ""; // clear
+                    dropdownMenu.classList.add('dropdown-menu_active');
 
-                    let items       = document.createElement('ul');
-                    items.className = 'dropdown-menu__items';
-                    items.innerHTML = `
-                        <li class='dropdown-menu__title'>${ elem.title }</li>
-                    `;
-                    dropdownMenu.appendChild(items);
+                    let elemsData = Object.entries(elemData);
 
-                    elem.items.forEach((e) => {
-                        let item       = document.createElement('li');
-                        item.className = 'dropdown-menu__item';
-                        item.innerHTML = `
-                        <a href='${e.url}'>${e.item}</a>
-                    `;
-                        items.appendChild(item);
+                    elemsData.forEach((elems) => {
+                        let blockContItems = document.createElement('ul');
+                        blockContItems.className = 'dropdown-menu__items';
+                        dropdownMenu.appendChild(blockContItems);
+
+                        elems[1].forEach((elem) => {
+                            let item       = document.createElement('li');
+                            item.className = 'dropdown-menu__item';
+                            item.innerHTML = `
+                                <a href='${ elem.url }'>${ elem.item }</a>
+                            `;
+                            blockContItems.appendChild(item);
+                        });
                     });
+                };
+                /* End Functions */
 
+                headerMenuItem.forEach((menuItem) => {
+                    menuItem.addEventListener('mouseover', (event) => {
+                        let target = event.target;
+
+                        switch (target.innerText) {
+                            case 'Shop':
+                                showSubmenuItems(data[0].mainitem1[0]);
+                                break;
+                            case 'Collections':
+                                showSubmenuItems(data[1].mainitem2[0]);
+                                break;
+                            default:
+                                dropdownMenu.classList.remove('dropdown-menu_active');
+                                break;
+                        }
+                    });
                 });
+
+                document.addEventListener('mouseover', (event) => {
+                    let target = event.target;
+                    if (!target.closest('.wrapper-header')){
+                        dropdownMenu.classList.remove('dropdown-menu_active');
+                    }
+                });
+
             }
         });
-
         request.send();
     };
     dropdownSubMenu();
